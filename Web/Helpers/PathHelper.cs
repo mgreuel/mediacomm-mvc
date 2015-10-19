50d344a75d7aa598ea882deec77f7c079d9c96c8
mgreuel <dotnet@greuel.name>
2015-10-19 22:52:30 +0200

    Added path helper

---
+++ Web/Helpers/PathHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MediaCommMvc.Web.Helpers
+{
+    public static class PathHelper
+    {
+        private static readonly char[] InvalidCharacters = Path.GetInvalidFileNameChars().Concat(Path.GetInvalidPathChars()).Distinct().ToArray();
+
+        private static readonly string InvalidCharactersPattern = $@"[{Regex.Escape(new string(InvalidCharacters) + " $.§ß%^&;=,'^´`#")}]";
+
+        private static readonly Regex InvalidCharactersRegex = new Regex(InvalidCharactersPattern);
+
+        public static string GetValidDirectoryName(string directoryName)
+        {
+            return InvalidCharactersRegex.Replace(directoryName, "_");
+        }
+    }
+}
